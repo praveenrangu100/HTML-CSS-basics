42370fbf0b11353fa9d1f8eb2e3072e01e52915a
Praveen Rangu <120257508+praveenrangu100@users.noreply.github.com>
2023-02-12 21:22:25 +0530

    Add files via upload

---
+++ criclogin.js
@@ -0,0 +1,36 @@
+function validatelogin(){
+    var name = document.getElementById("mailid");
+    var password = document.getElementById("pass");
+    text="";
+    if(password.value.indexOf(' ')>=0){
+        text="Enter valid password...";
+        document.getElementById("text2").innerHTML=text;
+        return false;
+    }
+    if(name.value.indexOf(' ')>=0){
+        text="Enter valid email...";
+        document.getElementById("text1").innerHTML=text;
+        return false;
+    }
+    if(name.value.trim()==null|| name.value.trim()==""){
+        text="Enter valid mailid...";
+        document.getElementById("text1").innerHTML=text;
+        return false;
+    }
+    else if(password.value.trim()==null || password.value.trim()==""){
+        text="Enter valid password...";
+        document.getElementById("text2").innerHTML=text;
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+function showpassword(){
+    var password =document.getElementById("pass");
+    if(password.type=="password"){
+        password.type="text";
+    }else{
+        password.type="password";
+    }
+}
